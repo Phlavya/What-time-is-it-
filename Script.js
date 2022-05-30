bd5fcb2083a0eef520327e2ea52e16f28c6ecae8
Flávia Braga <Bragarflavia@gmail.com>
2022-05-30 18:56:09 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,29 @@
 
+const getHoras = () => {
+
+  const clock = document.getElementsByClassName('clock')[0]
+
+  const data = nova data()
+
+  const horas = data.getHours()
+
+  const minutos = data.getMinutes()
+
+  const segundos = date.getSeconds()
+
+  const hora = horas < 10 ? `0${horas}`: horas
+
+  const minuto = minutos < 10 ? `0${minutos}`: minutos
+
+  const segundo = segundos < 10 ? `0${seconds}`: segundos
+
+  clock.innerHTML = `${hora}:${minuto}:${segundo}`
+
+}
+
+setInterval(() => {
+
+  getHoras()
+
+}, 1000)
+
